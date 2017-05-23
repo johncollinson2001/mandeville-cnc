a5798c610cbe3161fd65b3ba0b539e86ec9679dc
John Collinson <mail@johncollinson.info>
2017-05-23 22:46:14 +0100

    added basic gallery

--- src/MandevilleJoinery.Web/wwwroot/_references.js
+++ src/MandevilleJoinery.Web/wwwroot/_references.js
@@ -4,6 +4,7 @@
 /// <reference path="lib/bootstrap-material-design/dist/js/material.js" />
 /// <reference path="lib/bootstrap-material-design/dist/js/ripples.js" />
 /// <reference path="lib/bootstrap-sass/assets/javascripts/bootstrap.js" />
+/// <reference path="lib/fotorama/fotorama.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validate/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
